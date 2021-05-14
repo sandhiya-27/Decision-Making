2f30a8e5eb468a6ed73cdccb2889599ea6c27143
sandhiya-27 <82830715+sandhiya-27@users.noreply.github.com>
2021-05-14 23:11:39 +0530

    created voting eligibility  file
    This file prints the  checking process that  if a  person  is eligible to vote or not on the screen.

---
+++ voting eligibility.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int age;
+	cout<<"Enter age of a user:";
+	cin>>age;
+	
+	if(age>=18)
+	{
+		cout<<"\nYou are eligibile";
+	}
+	else
+	{
+		cout<<"\nYou are not eligible";
+	}
+return 0;
+}
